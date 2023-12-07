7a4c7029391ebcb7f1473755da536e9d6d06172e
Matt Guerrette <Matt.Guerrette@amd.com>
2023-12-07 11:29:22 -0500

    Fix docs theme issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,12 +84,12 @@ todo_include_todos = False
 
 # import sphinx_rtd_theme for retrieving theme
 # path from module
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = 'sphinx_rtd_theme'
 # html_theme = 'alabaster'
-# html_theme = 'classic'
+html_theme = 'classic'
 # html_theme = 'sphinxdoc'
 # html_theme = 'scrolls'
 # html_theme = 'agogo'
